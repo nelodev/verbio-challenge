502b8e0a1357f0dddd9bcc3d22b7a0f106dcc84c
Nelo Puchades Bresó <correo@nelo.dev>
2021-07-18 19:50:16 +0200

    Added interfaces for Messages component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,12 +1,17 @@
 import Message from './Message';
 
-function Messages({
-  messages,
-  className,
-}: {
-  messages: any[];
+interface messageProp {
+  type: string;
+  text: string;
+  url?: string;
+}
+
+interface messagesProps {
+  messages: messageProp[];
   className?: string;
-}) {
+}
+
+function Messages({messages, className}: messagesProps) {
   return (
     <div
       className={`flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch ${className}`}
