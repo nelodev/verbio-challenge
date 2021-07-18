5b8c68343d7b2cc32b769a1f45c7db13536b0a74
Nelo Puchades Bresó <correo@nelo.dev>
2021-07-18 18:04:40 +0200

    Fixed margin layout in all pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './index.css';
 import {Home, Login, Chat} from './pages/index';
-// import MarginLayout from './components/ui/MarginLayout';
+import MarginLayout from './components/ui/MarginLayout';
 
 function App() {
   return (
-    // <MarginLayout>
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <MarginLayout>
+            <Home />
+          </MarginLayout>
         </Route>
         <Route exact path="/login">
-          <Login />
+          <MarginLayout>
+            <Login />
+          </MarginLayout>
         </Route>
         <Route exact path="/chat">
           <Chat />
         </Route>
       </Switch>
     </Router>
-    // </MarginLayout>
   );
 }
 
